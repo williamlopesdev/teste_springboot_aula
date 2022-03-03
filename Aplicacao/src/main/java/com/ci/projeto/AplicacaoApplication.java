ec9c9df3a59748396de17281d259d41f243e7387
williamlopesdev <wilcypherr@gmail.com>
2022-03-03 11:02:22 -0300

    primeiro commit

---
+++ Aplicacao/src/main/java/com/ci/projeto/AplicacaoApplication.java
@@ -0,0 +1,13 @@
+package com.ci.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacaoApplication.class, args);
+	}
+
+}
---
+++ Aplicacao/src/test/java/com/ci/projeto/AplicacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ci.projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
